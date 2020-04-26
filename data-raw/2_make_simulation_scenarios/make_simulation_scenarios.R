bae2be3cfaf9f43530886af340f67964ead5cb8f
Jasper Ginn <jasperginn@gmail.com>
2020-04-26 10:48:47 +0200

    Update rerun data

--- data-raw/2_make_simulation_scenarios/make_simulation_scenarios.R
+++ data-raw/2_make_simulation_scenarios/make_simulation_scenarios.R
@@ -114,8 +114,7 @@ scen_rerun <- bind_rows(scen %>% filter(save_model), scen_subs %>% filter(save_m
 
 # Make new starting values & seeds for each model
 set.seed(22244214)
-# scen_rerun$dsim_seed <- sample(1:5678623, nrow(scen_rerun), replace=FALSE)
-scen_rerun$model_seed <- sample(1:5678623, nrow(scen_rerun), replace=FALSE)
+scen_rerun$model_seed <- sample(1:99999999, nrow(scen_rerun), replace=FALSE)
 
 # Create start values
 ## TPM gamma
@@ -134,24 +133,118 @@ start_gamma <- lapply(diag_value, function(x) {
 ## Emission distributions
 # For the 3 continuous emission distributions
 start_emiss <- lapply(1:nrow(scen_rerun), function(x) {
-  jsonlite::toJSON(list(
-    #EEG_Fpz_Cz_max_gamma
-    EEG_Fpz_Cz_mean_beta = c( -0.36 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                              -0.60 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                              0.7 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1)),
-    # EOG_median_theta
-    EOG_median_theta = c( 1.01 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                          -1.31 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                          -.24 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1)),
-    # EOG_min_beta
-    EOG_min_beta = c(  0.75 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                       -1.3 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1),
-                       0 + runif(1, -.2, .2), 0.2 + runif(1, -.1,.1))
-  ), pretty = TRUE, flatten = TRUE)
+  jsonlite::toJSON(
+    lapply(jsonlite::fromJSON(scen_rerun$start_emiss[x]), function(y) y + runif(6, -0.03, 0.03)),
+    pretty = TRUE,
+    flatten = TRUE
+  )
 })
 # Add to data
 scen_rerun$start_gamma <- as.character(start_gamma)
 scen_rerun$start_emiss <- as.character(start_emiss)
+
+# Test two models ----
+
+# Get scenario
+sim_rer <- scen_rerun %>%
+  filter(n==20,
+         n_t==800,
+         zeta == 0.25,
+         Q == 0.1) %>%
+  slice(1) %>%
+  as.list()
+
+# Model 2
+sim_orig <- scen %>%
+  filter(iteration_id == sim_rer$iteration_id) %>%
+  as.list()
+
+# Make dataset
+data_simulated <- simulate_dataset(sim_rer$n, sim_rer$n_t, sim_rer$zeta, sim_rer$Q, sim_rer$dsim_seed)
+# To data frame
+tdf <- data.frame(
+  id = data_simulated$obs[,1],
+  EEG_mean_beta = data_simulated$obs[,2],
+  EOG_median_theta = data_simulated$obs[,3],
+  EOG_min_beta = data_simulated$obs[,4]
+)
+states <- data_simulated$states[,2]
+
+# Plot
+tdf %>%
+  mutate(state = states) %>%
+  gather(var, val, -id, -state) %>%
+  ggplot(aes(x=val, fill = as.factor(state))) +
+  geom_density(alpha=0.4) +
+  facet_wrap(". ~ var")
+# Get summary statistics for each
+em1 <- tapply(tdf[,-1]$EEG_mean_beta, states, mean)
+em2 <- tapply(tdf[,-1]$EOG_median_theta, states, mean)
+em3 <- tapply(tdf[,-1]$EOG_min_beta, states, mean)
+hyp_priors <- list(
+  as.vector(em1[sort.list(as.numeric(names(em1)))]),
+  as.vector(em2[sort.list(as.numeric(names(em2)))]),
+  as.vector(em3[sort.list(as.numeric(names(em3)))])
+)
+# Reshape start values (model 1)
+sim_rer$start_gamma <- jsonlite::parse_json(sim_rer$start_gamma)
+sim_rer$start_emiss <- jsonlite::parse_json(sim_rer$start_emiss)
+m <- sqrt(length(sim_rer$start_gamma$tpm))
+start_values1 <- list(
+  matrix(unlist(sim_rer$start_gamma$tpm), nrow=m, ncol=m,
+         byrow = TRUE),
+  matrix(unlist(sim_rer$start_emiss$EEG_Fpz_Cz_mean_beta),
+         ncol=2, byrow = TRUE),
+  matrix(unlist(sim_rer$start_emiss$EOG_median_theta),
+         ncol=2, byrow = TRUE),
+  matrix(unlist(sim_rer$start_emiss$EOG_min_beta),
+         ncol=2, byrow=TRUE)
+)
+# Run model
+mod1 <- sleepsimR::run_mHMM(tdf, start_values = start_values1,
+                            mprop = list("m" = 3, "n_dep" = 3),
+                            hyperprior_means = hyp_priors,
+                            model_seed = sim_rer$model_seed,mcmc_iterations=3250, mcmc_burn_in = 1250,
+                            order_data = FALSE)
+
+# Reshape start values
+sim_orig$start_gamma <- jsonlite::parse_json(sim_orig$start_gamma)
+sim_orig$start_emiss <- jsonlite::parse_json(sim_orig$start_emiss)
+m <- sqrt(length(sim_orig$start_gamma$tpm))
+start_values2 <- list(
+  matrix(unlist(sim_orig$start_gamma$tpm), nrow=m, ncol=m,
+         byrow = TRUE),
+  matrix(unlist(sim_orig$start_emiss$EEG_Fpz_Cz_mean_beta),
+         ncol=2, byrow = TRUE),
+  matrix(unlist(sim_orig$start_emiss$EOG_median_theta),
+         ncol=2, byrow = TRUE),
+  matrix(unlist(sim_orig$start_emiss$EOG_min_beta),
+         ncol=2, byrow=TRUE)
+)
+
+# Run model
+mod2 <- sleepsimR::run_mHMM(tdf, start_values = start_values,
+                            mprop = list("m" = 3, "n_dep" = 3),
+                            hyperprior_means = hyp_priors,
+                            model_seed = sim_orig$model_seed,mcmc_iterations=3250, mcmc_burn_in = 1250,
+                            order_data = FALSE)
+
+sleepsimReval::tpp(mod1, mod2, "emiss_mu_bar", var=1)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_mu_bar", var=1)
+sleepsimReval::tpp(mod1, mod2, "emiss_mu_bar", var=2)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_mu_bar", var=2)
+sleepsimReval::tpp(mod1, mod2, "emiss_mu_bar", var=3)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_mu_bar", var=3)
+
+sleepsimReval::tpp(mod1, mod2, "emiss_varmu_bar", var=1)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_varmu_bar", var=1)
+sleepsimReval::tpp(mod1, mod2, "emiss_varmu_bar", var=2)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_varmu_bar", var=2)
+sleepsimReval::tpp(mod1, mod2, "emiss_varmu_bar", var=3)
+sleepsimReval::compute_grs(mod1, mod2, "emiss_varmu_bar", var=3)
+
+sleepsimReval::tpp(mod1, mod2, "gamma_int_bar", var=1)
+sleepsimReval::compute_grs(mod1, mod2, "gamma_int_bar", var=1)
 
 # Save
 write.csv(scen_rerun, file=gzfile("data-raw/2_make_simulation_scenarios/scenarios_rerun.csv.gz"))
@@ -159,4 +252,4 @@ write.csv(scen_rerun, file=gzfile("data-raw/2_make_simulation_scenarios/scenario
 # Save as datasets in this library
 usethis::use_data(scen)
 usethis::use_data(scen_subs)
-usethis::use_data(scen_rerun)
+usethis::use_data(scen_rerun, overwrite = TRUE)
